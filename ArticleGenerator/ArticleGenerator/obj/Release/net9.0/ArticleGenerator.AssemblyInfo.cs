ab7d4345f38b942dd239b3715fc32d2a03928e73
John Florinus <johnflorinus@gmail.com>
2025-09-21 13:26:54 +0200

    added ad blocker recovery

--- ArticleGenerator/ArticleGenerator/obj/Release/net9.0/ArticleGenerator.AssemblyInfo.cs
+++ ArticleGenerator/ArticleGenerator/obj/Release/net9.0/ArticleGenerator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArticleGenerator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f57e452c228bf92cf1a03add24b6891f9facb81")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a33b4b8c6661dc313e8c6f50d8c9a2d126a6d13")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArticleGenerator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArticleGenerator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
